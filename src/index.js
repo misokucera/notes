f5c572f6b5c2b97ffc30be84552546a936e0a55e
misok <miso.kucera97@gmail.com>
2018-07-01 13:27:08 +0200

    firebase config moved

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from "firebase";
 import "./index.css";
+import firebaseConfig from './firebaseConfig';
 
-var config = {
-    apiKey: "AIzaSyCwSYqDST9fMa0SNPW6OJAhBB1AbT_F2JM",
-    authDomain: "dashboard-6d455.firebaseapp.com",
-    databaseURL: "https://dashboard-6d455.firebaseio.com",
-    projectId: "dashboard-6d455",
-    storageBucket: "dashboard-6d455.appspot.com",
-    messagingSenderId: "765132708082"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
